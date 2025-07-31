c702f75acec43bb0c872db4e8eddc45b1a565977
sridharanikn-cloud <sridharanikan@gmail.com>
2025-07-31 15:57:12 +0530

    Create mark.py

---
+++ mark.py
@@ -0,0 +1,13 @@
+num=int(input("enter a mark"))
+if num>=90:
+    print("A grade")
+elif num>=80 or num<90:
+    print("B grade")
+elif num>=70 or num<80:
+    print("C grade")
+elif num>=60 or num<70:
+    print("D grade")
+elif num>=50 or num<60:
+    print("E grade")
+else:
+    print("F grade")
